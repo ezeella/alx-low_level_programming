54e0e64fed724f03b2252dae469331154fc56ca2
ezeella <myrealface1@gmail.com>
2022-03-10 15:09:32 -0800

    sdfsdfs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
  * Programming is like building a multilingual puzzle
  * Return:0 if exited properly, non-zero otherwise
  */
+
  int main(void)
 {	
-   puts("Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
    return (0);
 }
