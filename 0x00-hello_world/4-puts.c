71c57f7e1afebb017678bfdc81839f915fd7ad85
ezeella <myrealface1@gmail.com>
2022-03-10 14:14:26 -0800

    mine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main -print the string in the put function
- *   
+ * main -print the string in the put function   
  * Description:using the main function
- * this program prints "programming is
- * like building a multilingual puzzle
+ * this program prints
+ * Programming is like building a multilingual puzzle
  *
- * Return:0
+ * Return:0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 
 {	
