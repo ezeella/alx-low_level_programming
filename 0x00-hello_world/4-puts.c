3fc3a7dfc5ed4b0281758b8f528a9e3922fd52b0
ezeella <myrealface1@gmail.com>
2022-03-10 14:45:26 -0800

    sfsfs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-/**
+ #include <stdio.h>
+ /**
  * main -print the string in the put function   
  * Description:using the main function
  * this program prints
  * Programming is like building a multilingual puzzle
  *
- * Return:0 if exited properly, non-zero otherwise
+ * Return 0 if exited properly, non-zero otherwise
  */
  int main(void){	
-   puts("\"Programming is like building a multilingual puzzle");
+   puts("Programming is like building a multilingual puzzle");
    return (0);
 }
