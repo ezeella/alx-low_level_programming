337df3e472572fdc4a0529890b78e46c554d5d0e
ezeella <myrealface1@gmail.com>
2022-03-10 14:04:50 -0800

    mine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,16 +5,15 @@
  * Description:using the main function
  * this program prints "programming is
  * like building a multilingual puzzle
-
-
+ *
  * Return:0
  */
 
 int main(void)
 
 {	
-  puts("\"Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+ return (0);
 
 }
