a7e302dac2b36ff653ccc2260b861cf7c177ba31
ezeella <myrealface1@gmail.com>
2022-03-11 16:04:51 -0800

    whatdjdk

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/* more headers goes there */
-/* betty style doc for function main goes there */
-/* Description: check if a number is positive or negative */
+#include <stdio.h>
+/**
+ * main - Determines if a number is positive, negative or zero.
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
