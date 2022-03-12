45ec709c3057f4283550816e97e7460e98137b7a
ezeella <myrealface1@gmail.com>
2022-03-11 17:51:57 -0800

    added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char emmanuella[26] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i < 26; i++)
+{
+	if (emmanuella[i] != "q" && emmanuella[1] != "e")
+	{
+		putchar(emmanuella[i]);
+	}
+}
+putchar('\n');
+return (0);
+}
