744f566fa3b606681fb97f8adbfb2f7925201cf0
ezeella <myrealface1@gmail.com>
2022-03-18 12:16:10 -0700

    _isppercase function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,10 @@
 
 /**
  * main  _isupper - checks if parameter is an uppercase character.
- * description on _isuper function is a function that  returns ``:wq!
- * i or zero if uppcase or lowercase
- * @c: input character.
+ * description: this function return i if the uppercase and 0 if lower case
+ * @c: input number.
  * Return: 1 if is an uppercase character, 0 in other case.
- * description here oeepepepep
  */
-
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
