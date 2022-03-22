537d42e01b528eccc47f2b311cc9562765c0ae95
ezeella <myrealface1@gmail.com>
2022-03-22 12:08:51 -0700

    mine

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int total = 0;
 	char null_flag = 0;
-	
+
 	while (*s)
 	{
 		if (*s == '-')
@@ -21,12 +21,12 @@ int _atoi(char *s)
 			null_flag = 1;
 			total = total * 10 + *s - '0';
 		}
-	
+
 		else if (null_flag)
 			break;
 		s++;
 	}
-	
+
 	if (sign < 0)
 		total = (-total);
 
